abdbf1221de43dcceb66eb3a9526312b2a58eab9
Vladimir <likholetov.vladimir2@yandex.ru>
2024-05-09 21:10:58 +0300

    final ver of practice in folder 20

--- src/20_conditions/script.js
+++ src/20_conditions/script.js
@@ -18,16 +18,16 @@ if (1 == 2) {
 // (num === 50) ? console.log('Ok!') : console.log('Error');
 
 
-const num = 51;
+const num = '50';
 
 switch (num) {
-  case 49:
+  case '49':
     console.log('Неверно');
     break;
-  case 100:
+  case '100':
     console.log('Неверно');
     break;
-  case 50:
+  case '50':
     console.log('В точку!');
     break;
   default:
