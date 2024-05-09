60b20d9a7f030d51d4dec37e68394a662cc3328d
Vladimir <likholetov.vladimir2@yandex.ru>
2024-05-09 23:00:59 +0300

    modify script.js in folder 21

--- src/21_logOperators/script.js
+++ src/21_logOperators/script.js
@@ -1,10 +1,32 @@
 "use strict";
 
-const hamburger = true;
-const fries = true;
+// const hamburger = 5;
+// const fries = null;
 
-if (hamburger && fries) {
-  console.log('Я сыт!');
+// if (hamburger && fries) {
+//   console.log('Я сыт!');
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null & 5);
+// console.log(0 && 'knjbjbbhbada');
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+  console.log('Все довольны!');
+} else {
+  console.log('Мы уходим');
 }
 
-// (hamburger && fries) ? console.log('Я сыт!'): console.log('Я голоден!');
+console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!0);
